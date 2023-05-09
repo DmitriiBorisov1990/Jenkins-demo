deebc6d3b34b63d6f3873b30d573bf7ef8b9b578
Dmitrii Borisov <dmitrii.borisov1990@gmail.com>
2023-05-09 18:23:19 +0300

    add TestInstance.Lifecycle.PER_CLASS

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CalculatorTest {
 
     private Calculator calculator;
